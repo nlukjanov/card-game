a0be2c1ff77e55eb02c23a0b5b499a91e46d5d57
Nikita Lukjanov <nlukjanov@gmail.com>
2023-04-04 17:53:38 +0100

    feat: add draw message

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-// create card deck
 const deck = Array.from({ length: 52 }, (_, i) => i + 1);
-// shuffle deck
 
 function shuffleDeck(deck: number[]): number[] {
   for (let i = deck.length - 1; i > 0; i--) {
@@ -25,7 +23,7 @@ function playGame() {
     const player1Card = player1Stack.pop()!;
     const player2Card = player2Stack.pop()!;
 
-    console.log(`Round ${round}:`);
+    console.log(`\nRound ${round}:`);
     console.log(`Player 1: ${player1Card} - Player 2: ${player2Card}`);
 
     if (player1Card > player2Card) {
@@ -42,8 +40,10 @@ function playGame() {
 
   if (player1Score > player2Score) {
     console.log('Player 1 wins the game!');
-  } else {
+  } else if (player2Score > player1Score) {
     console.log('Player 2 wins the game!');
+  } else {
+    console.log('It is a draw!');
   }
 }
 
